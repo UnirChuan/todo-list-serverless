272d79c37d8440983ceb936ea9dc2207f8dc57f7
EC2 Default User <ec2-user@ip-172-31-75-252.ec2.internal>
2021-01-07 16:06:35 +0000

    sube

---
+++ todos/traslete.py
@@ -0,0 +1,44 @@
+import json
+import os
+import boto3
+from todos import decimalencoder
+
+dynamodb = boto3.resource('dynamodb')
+translate = boto3.client('translate')
+comprehend = boto3.client('comprehend')
+
+
+def get(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch all todos from the database
+    result = table.get_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+    target = event['pathParameters']['lang']
+    task =  result['Item']['test']
+    source_lenguage = detect_language(task)
+    
+    source = source_lenguage['Languages'][0]['LanguageCode']
+    
+    traslated = translate_text(task, source, target)
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result['Items'], cls=decimalencoder.DecimalEncoder)
+    }
+
+    return response
+
+def detect_language(task):
+    response = comprehend.detect_dominant_language(Text='string')
+    return response
+
+def translate_text(task, source, target):
+    
+    response = translate.translate_text(Test=task, SourceLanguageCode=source , TargetLanguageCode=target)
+    
+    return response
